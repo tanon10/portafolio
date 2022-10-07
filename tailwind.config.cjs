ea2ead8997c52cecfcca8b38430ddb4d7a8569a5
tanon10 <antoniocaldas3101@gmail.com>
2022-10-07 12:53:50 -0500

    add tailwind and font

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        mplus: ["'M PLUS Rounded 1c'", "Verdana", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
