9df1bd4c08d0c24eec5db46d5a86f6b0ff5e9120
tanon10 <antoniocaldas3101@gmail.com>
2022-10-07 22:46:02 -0500

    add header

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Portafolio - Tanon10';
+export const SITE_DESCRIPTION = 'Conoce un poco sobre mí, mis proyectos, mis conocimientos';
+export const HOMEPAGE_URL='www.tanon10.xyz'
