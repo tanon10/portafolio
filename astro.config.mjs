7c00e1cbd0fc54a4ea8db9cb1c3c2f7d87b520c2
tanon10 <antoniocaldas3101@gmail.com>
2022-10-09 17:08:54 -0500

    test deploy 0.0.3

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import addClases from "rehype-add-classes";
 // https://astro.build/config
 export default defineConfig({
   site: "https://tanon10.github.io",
-  base: "/portafolio",
   integrations: [sitemap(), react(), image(), tailwind()],
   markdown: {
     extendDefaultPlugins: true,
